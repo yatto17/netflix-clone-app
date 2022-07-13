2c3f034b9844d7b1ef6b8a524b2b2f63bec1b155
yatto17 <ysht17@gmail.com>
2022-07-13 13:46:00 +0900

    YouTubeトレイラーの表示

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 
 import axios from "../axios";
 import "./Row.scss";
@@ -18,11 +19,22 @@ type Movie = {
     original_name: string;
     poster_path: string;
     backdrop_path: string;
-}
+};
+
+// trailerのoption
+type Options = {
+    height: string;
+    width: string;
+    playerVals: {
+        autoplay: 0 | 1 | undefined;
+    };
+};
 
 export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     const [movies, setMovies] = useState<Movie[]>([]);
+    const [trailerUrl, setTrailerUrl] = useState<string | null>('');
 
+    // urlが更新されるたびに
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
@@ -33,7 +45,25 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
         fetchData();
     }, [fetchUrl]);
 
-    console.log(movies);
+    const opts: Options = {
+        height: "390",
+        width: "640",
+        playerVals: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = async (movie: Movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            let trailerurl = await axios.get(
+                `/tv/${movie.id}/videos?api_key=${process.env.REACT_APP_TMDB_API_KEY}`
+            );
+            setTrailerUrl(trailerurl.data.results[0]?.key);
+        }
+    };
 
     return(
         <div className="Row">
@@ -48,9 +78,11 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
                             isLargeRow ? movie.poster_path : movie.backdrop_path
                         }`}
                         alt={movie.name}
+                        onClick={() => handleClick(movie)}
                     />
                 ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     );
 };
